8e9e0aaac2f36caa7f78d05aee411c476fb40175
Mang0st <ramcioff@gmail.com>
2022-12-26 13:24:27 +0500

    v3

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('registration', views.registration, name='registration'),
+    path('profile/', views.profile, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from .models import User
-from .forms import UserLoginForm, UserRegistrationForm
+from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth
 from django.urls import reverse
 
@@ -37,3 +37,17 @@ def registration(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user,
+                               data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    return render(request, 'users/profile.html', {'form': form})
